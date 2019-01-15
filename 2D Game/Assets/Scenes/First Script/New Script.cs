72c9fd690f709190e85e9d553ec33d83db97e9b8
Megan Petersen <petersen.megan9646@gmail.com>
2019-01-14 17:18:58 -0700

    First Script Test in Unity

---
+++ 2D Game/Assets/Scenes/First Script/New Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour {
+
+public int WaterBottle = 60;
+   
+    // Use this for initialization
+	void Start () {
+		print(WaterBottle);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+}
