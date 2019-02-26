f37f98b1ad6cf522389a0f50990d3cfaa82a715c
Megan Petersen <petersen.megan9646@gmail.com>
2019-02-25 17:06:59 -0700

    If's, else if's and else statement practice

---
+++ 2D Game/Assets/Scripts/Assignments/If_Statements.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If_Statements : MonoBehaviour{
+
+    public string stopLight;
+    public bool isUtahn = true;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(stopLight == "Red"){
+            //blockof code that is run if the condition is met
+            // ! means NOT
+            
+            if(isUtahn){
+               print("Stop! The light is Red!"); 
+            }
+            
+        }
+        else if(stopLight == "Yellow"){
+            print("You've got this, bruh!");
+
+        }
+        else if(stopLight == "Green"){
+            
+        }
+        else{
+             print("Check phone, turn up radio and sit at light until almost yellow, then go!");
+        }
+    
+    }
+}
