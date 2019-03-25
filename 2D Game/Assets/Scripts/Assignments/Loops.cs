300b1b831a5475f210dd7694d74aeaee9a2475e6
Megan Petersen <petersen.megan9646@gmail.com>
2019-03-25 17:54:44 -0600

    Added Enemy, Fixed Issues and Errors

---
+++ 2D Game/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+    public int bottles = 100;
+    public bool isRaining = true;
+   
+    // Start is called before the first frame update
+    void Start(){
+        
+
+        //For loop
+        //for(int i=0; i <= bottles; i++){  //i=0 is where the loop begins each time
+            //print(i +"bottles of root beer on the wall");
+
+    // Loops work well with things like objects such as crates that need to loop and come back each time
+        //}  
+
+        //while loop
+            while(bottles <= 100){
+                print(bottles + "bottles of root beer on the wall");
+                bottles ++;
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D Game/Assets/Scripts/Enemy_Patrol.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Patrol : MonoBehaviour{
+
+public bool notAtEdge;
+public Transform edgeCheck;
+
+public Transform wallCheck;
+public float wallCheckRadius;
+public LayerMask whatIsWall;
+private bool hittingWall;
+
+public float moveSpeed;
+public bool moveRight;
+public Vector3 scale;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = transform.localScale;
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+        if(hittingWall || !notAtEdge){
+            moveRight = !moveRight;
+
+        }
+         if(moveRight){
+             transform.localScale = new Vector3(-scale.x,scale.y,scale.z);
+             GetComponent<Rigidbody2D>(). velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y); //moving right
+            }
+
+         else {
+            transform.localScale = new Vector3(scale.x,scale.y,scale.z);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y); //moving left
+             
+            }
+                
+    }
+}
