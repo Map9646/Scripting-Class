254d2145c0fd4df62630430ceabf5d11e23c7e41
Megan Petersen <petersen.megan9646@gmail.com>
2019-04-01 17:16:31 -0600

    Created Level Manager Script and Particles

---
+++ 2D Game/Assets/Scripts/Level_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_Manager : MonoBehaviour{
+
+//Look up code and how it works and add it to game and player since I missed last class. :) 
+//Put Level Manager on Level Manager Object in game when you bring script into the game
+//Make a Kill Player Script as well and place it on the enemy
+
+        
+
+    // Start is called before the first frame update
+    void Start(){
+
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        
+    }
+}
