41a18cdabf0e5442e9ef33d24456deaa3512a2d0
Megan Petersen <petersen.megan9646@gmail.com>
2019-04-04 14:10:29 -0600

    Finished Kill Player Script and Started HealthBar

---
+++ 2D Game/Assets/Scripts/Health_Bar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_Bar : MonoBehaviour{
+
+    public int healthBar;
+    public int currentHP;
+    
+
+
+    // Start is called before the first frame update
+    void Start(){
+
+        healthBar = 100;
+
+        currentHP = healthBar;
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    public void damageTaken(){
+        currentHP -= damageTaken;
+
+        if(currentHP <= 0){
+            currentHP <= 0;
+            print("Game Over!");
+        }    
+        
+    }
+
+
+         
+    }
+    
+
--- 2D Game/Assets/Scripts/KillPlayer.cs
+++ 2D Game/Assets/Scripts/KillPlayer.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillPlayer : MonoBehaviour
-{
+public class KillPlayer : MonoBehaviour{
+
+    public Level_Manager levelManager;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+
+        levelManager = FindObjectOfType<Level_Manager>();
         
     }
 
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.name == "Player"){
+            levelManager.RespawnPlayer();
+        }
+    }
+
+
+
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+
         
     }
 }
