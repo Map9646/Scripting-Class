2a52a8d868e653441b9edc674168f66f3d4d1dbf
Megan Petersen <petersen.megan9646@gmail.com>
2019-04-08 17:17:31 -0600

    Added Projectile and Player Shooter Script

---
+++ 2D Game/Assets/Scripts/Player_Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Shoot : MonoBehaviour{
+    public Transform firePoint;
+    public GameObject projectile; //once you figure out what you want player to throw, replace projectile with that
+
+    //Remember to rename projectile once I know exactly what it will be. 
+
+
+
+    // Start is called before the first frame update
+    void Start(){
+
+        projectile = Resources.Load("Prefabs/Projectile") as GameObject;
+        //Error will show in Unity only because we have nothing in prefabs by that name yet
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            Instantiate(projectile,firePoint.position,firePoint.rotation);
+            //The object we create in Unity will be linked or inherited through the Player, so they have to be on the same field
+
+        
+    }
+}
---
+++ 2D Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour{
+    public float Speed;
+    public float timeOut;
+    public GameObject Player;
+    public GameObject enemyDeath;
+    public GameObject projectileParticle; //For if it hits something other than the enemy, such as a wall
+    public int pointsForKill; 
+
+
+    // Start is called before the first frame update
+    void Start(){
+        Player = GameObject.Find("Player");
+
+        enemyDeath = Resources.Load("Prefabs/Death_PS") as GameObject;
+
+        projectileParticle = Resources.Load("Prefabs/ Respawn_PS") as GameObject;
+
+        if(Player.transform.localScale.x < 0){ //Ensures that projectile shoots the same direction as the player in facing 
+            Speed = -Speed;
+            //if you have only one single line after an if statement you don't have to add bracets, but you can if you want for practice
+        }
+            
+        Destroy(gameObject, timeOut); // Will destry the projectile object after a certain amount of time
+    }
+
+    // Update is called once per frame
+    void Update(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Enemy"){
+            Instantiate(enemyDeath, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+            ScoreManager.AddPoints(pointsForKill);
+        }
+
+        Destroy(gameObject); //Destroys projectile after hitting enemy. Otherwise it would just keep going after it hit the enemy
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        Instantiate(projectileParticle, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+    
+
+
+
+
+
+}
