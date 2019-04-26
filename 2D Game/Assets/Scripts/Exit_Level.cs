684ba0353772665717739c9784b8de8d380b4b90
Megan Petersen <petersen.megan9646@gmail.com>
2019-04-25 22:08:13 -0600

    Tried Adding an Exit Level. Might be easier to go back to Main Menu

--- 2D Game/Assets/Scripts/Exit_Level.cs
+++ 2D Game/Assets/Scripts/Exit_Level.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Exit_Level : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Exit_Level : MonoBehaviour{
+
+public int levelToLoad;
+    
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            
+        SceneManager.LoadScene(levelToLoad);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void LevelExit(){
+        Application.Quit();   
     }
 }
+
+    
